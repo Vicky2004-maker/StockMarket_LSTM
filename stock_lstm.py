e9c59b65ff628b14293d235137a52ad69c4c5792
Viknesh <vickygamingoff2004@gmail.com>
2023-12-01 00:48:33 +0530

    Completed LSTM

--- stock_lstm.py
+++ stock_lstm.py
@@ -1,15 +1,11 @@
-import keras.optimizers
 import yfinance as yf
-import pandas as pd
+
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 
 import tensorflow as tf
 from keras import Sequential
 from keras.layers import Dense, Dropout, CuDNNLSTM
-
-import joblib
 
 # %%
 
@@ -39,38 +35,27 @@ X, y = data['Open'].to_numpy().reshape((-1, 1)), data['Close'].to_numpy().reshap
 
 print(X.shape, y.shape)
 
-# %%
+# %% LSTM
+model = Sequential([
+    CuDNNLSTM(units=100, return_sequences=True, input_shape=(X.shape[0], 1), name='LSTM-1'),
+    Dropout(0.15, name='DROPOUT-1'),
 
-force_train = True
-file_name = f"D:/PyCharm_Projects/StockMarket/lstm_{stock_name}.model"
+    CuDNNLSTM(units=1000, return_sequences=True, name='LSTM-2'),
+    Dropout(0.15, name='DROPOUT-2'),
 
-if (not os.path.isfile(file_name)) or force_train:
-    model = Sequential([
-        CuDNNLSTM(units=100, return_sequences=True, input_shape=(X.shape[0], 1), name='LSTM-1'),
-        Dropout(0.15, name='DROPOUT-1'),
+    CuDNNLSTM(units=1000, return_sequences=True, name='LSTM-3'),
+    Dropout(0.15, name='DROPOUT-3'),
 
-        CuDNNLSTM(units=1000, return_sequences=True, name='LSTM-2'),
-        Dropout(0.15, name='DROPOUT-2'),
+    CuDNNLSTM(units=100, return_sequences=True, name='LSTM-4'),
+    Dropout(0.15, name='DROPOUT-4'),
 
-        CuDNNLSTM(units=1000, return_sequences=True, name='LSTM-3'),
-        Dropout(0.15, name='DROPOUT-3'),
+    Dense(units=1, name='DENSE-1')
+], name='SEQUENTIAL-1')
 
-        CuDNNLSTM(units=100, return_sequences=True, name='LSTM-4'),
-        Dropout(0.15, name='DROPOUT-4'),
-
-        Dense(units=1, name='DENSE-1')
-    ], name='SEQUENTIAL-1')
-
-    model.compile(optimizer=tf.optimizers.Adam(), loss=tf.losses.mean_squared_logarithmic_error,
-                  metrics=[tf.metrics.mean_absolute_percentage_error])
-    history = model.fit(X, y, epochs=150, use_multiprocessing=True, shuffle=False)
-
-    joblib.dump(model, filename=file_name)
-elif os.path.isfile(file_name) and (not force_train):
-    model = joblib.load(file_name)
-    history = model.history
-
-# %%
+model.compile(optimizer=tf.optimizers.Adam(), loss=tf.losses.mean_squared_logarithmic_error,
+              metrics=[tf.metrics.mean_absolute_percentage_error])
+history = model.fit(X, y, epochs=250, use_multiprocessing=True, shuffle=False)
+# %% LSTM - Summary and Visualization
 
 # noinspection PyUnboundLocalVariable
 model.summary()
@@ -83,7 +68,7 @@ metrics = [loss, mae]
 plt.close()
 plt.figure(figsize=(12, 12))
 count = 1
-for i in ['Loss', 'MAPE']:
+for i in ['MSLE', 'MAPE']:
     plt.subplot(2, 1, count)
     plt.plot(metrics[count - 1], label=f'min:{np.min(metrics[count - 1]):.4f}\nmax:{np.max(metrics[count - 1]):.4f}')
     plt.legend()
@@ -95,11 +80,15 @@ for i in ['Loss', 'MAPE']:
 plt.tight_layout()
 plt.show()
 
-# %%
+# %% Test - Predictions LSTM and its Visualization
+test_val = y.copy()
+y_pred = model.predict(test_val)
+y_pred = np.array(y_pred).reshape((-1))
+print(y_pred)
 
-
-test_val = 100
-val = np.array(test_val).reshape((-1, 1))
-y_pred = model.predict(val)
-print(test_val, y_pred)
-
+plt.figure(figsize=(10, 10))
+plt.title('LSTM - Actual vs Predictions')
+plt.plot(np.array(y).reshape((-1)), label='Actual')
+plt.plot(y_pred, label='Predicted')
+plt.legend()
+plt.show()
